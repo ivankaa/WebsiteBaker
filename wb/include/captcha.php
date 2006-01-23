6960a49e4c79aec90f3cb1681298ff98b5e27578
stefan <stefan@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2006-01-23 22:28:19 +0000

    Fixed ticket #107 - captcha being cached.
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@287 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/include/captcha.php
+++ wb/include/captcha.php
@@ -59,10 +59,24 @@ if(extension_loaded('gd') AND function_exists('imageCreateFromJpeg') AND isset($
 		imagestring($image, $fnt, $x, $y, substr($_SESSION['captcha'], $i, 1), $darkgray); 
 	}
 	
-	header('Content-type: image/png');
+	// start buffering for size determination
+	ob_start();
+	// add no cache headers
+	header("Expires: Mon, 1 Jan 1990 05:00:00 GMT");
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+	header("Content-type: image/png");
+	// Make image
 	imagepng($image);
+	// Fetch length
+	header("Content-Length: " . ob_get_length());
+	// send image and turn off buffering
+	ob_end_flush();
+	// clear memory
 	imagedestroy($image);
-	
+
 }
 
 ?>
