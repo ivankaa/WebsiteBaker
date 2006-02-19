639f34d6d5dd8bbf2c372a4969d701170bb0afff
ryan <ryan@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2006-02-19 05:31:10 +0000

    Updated all copyright notices to include 2006
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@310 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/admin/interface/index.php
+++ wb/admin/interface/index.php
@@ -1,11 +1,11 @@
 <?php
 
-// $Id: index.php,v 1.2 2005/06/22 05:28:39 rdjurovich Exp $
+// $Id$
 
 /*
 
  Website Baker Project <http://www.websitebaker.org/>
- Copyright (C) 2004-2005, Ryan Djurovich
+ Copyright (C) 2004-2006, Ryan Djurovich
 
  Website Baker is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
