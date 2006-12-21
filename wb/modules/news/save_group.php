5e5163bdef1438bda28920391389095284da5d1f
Ruebenwurzel <Ruebenwurzel@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2006-12-21 05:26:20 +0000

    Code cleaning and fixing two issues in save_group.php
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@391 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/modules/news/save_group.php
+++ wb/modules/news/save_group.php
@@ -57,11 +57,8 @@ if(isset($_FILES['image']['tmp_name']) AND $_FILES['image']['tmp_name'] != '') {
 	$filename = $_FILES['image']['name'];
 	$new_filename = WB_PATH.MEDIA_DIRECTORY.'/.news/image'.$group_id.'.jpg';
 	// Make sure the image is a jpg file
-	$file3=substr($filename, -3, 3);
 	$file4=substr($filename, -4, 4);
-	//echo $file3;
-	//echo $file4;
-	if(($file4 != '.jpg')and($file4 != '.JPG')and($file4 != '.png')and($file4 != '.PNG') and ($file4 !='.jpeg') and ($file4 != '.JPEG')) {
+	if(($file4 != '.jpg')and($file4 != '.JPG')and($file4 != '.png')and($file4 != '.PNG') and ($file4 !='jpeg') and ($file4 != 'JPEG')) {
 		$admin->print_error($MESSAGE['GENERIC']['FILE_TYPE'].' JPG (JPEG) or PNG a');
 	} elseif(
 	(($_FILES['image']['type']) != 'image/jpeg' AND mime_content_type($_FILES['image']['tmp_name']) != 'image/jpg') 
