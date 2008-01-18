f58a95f72af754dc90a2564315017bbe5b4254af
Ruebenwurzel <Ruebenwurzel@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2008-01-18 23:16:26 +0000

    changed version number of form modul
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@564 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/modules/form/info.php
+++ wb/modules/form/info.php
@@ -28,50 +28,10 @@ The Website Baker Project would like to thank Rudolph Lartey <www.carbonect.com>
 for his contributions to this module - adding extra field types
 */
 
-/*
-revision:
-2.6.2.0 21 jan 2007
-	minor fixes in the search tabel entries
-2.6.1.9 24 dez 2006
-	adapted all files to be compatible with 2.6.5
-2.6.1.8 03 oct 2006
-	fixed double )) in save_field.php line 76
-2.6.1.7 24 jan 2006
-	added header to modify_fields
-	removed a typo (captcha)
-2.6.1.6 23 jan 2006
-	added exit(0); after each header("Location: ...) call
-	added language text for 'none' thanks to Ruebenwurzel
-2.6.1.5 23 jan 2006
-    added anti caching to captcha
-    changed the way thankyou page works, if chosen page is none, mail still could be sent, thank you message is shown!
-    
-2.6.1.4 22 jan 2006
-    added email to mailto/from selected fields  -> Ticket #118 
-    removed last separator in radiobutton and checkbox 
-2.6.1.3 21 jan 2006
-    restored thankyou text
-    fixed mail lines, format of maillines -> Ticket #86
-    fixed max_submissions -> Ticket #115
-    fixed mailto/from selected fields (only textfields) -> Ticket #118 
-2.6.1.2 16 jan 2006
-    fixed ' in websitetitle
-2.6.1.1 14 jan 2006
-    changed thank you text to thank you page 
-    added thank you mail
-    changed behaviour of adding fields
-*/
-/*
-todo:
-- display of fieldlengths as variable, 
-- back after error with filled in stuff?
-- validation link for submissions???
-*/ 
-
 $module_directory = 'form';
 $module_name = 'Form';
 $module_function = 'page';
-$module_version = '2.6.2.0';
+$module_version = '2.7';
 $module_platform = '2.6.x';
 $module_author = 'Ryan Djurovich & Rudolph Lartey - additions John Maats - PCWacht';
 $module_license = 'GNU General Public License';
