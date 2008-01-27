a52a88b0dc7a745c778cff5bb14bbab3f054ae61
thorn <thorn@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2008-01-27 00:03:04 +0000

    CAPTCHA ttf_image: added variant. Adjusted character-pool and one background for better readability.
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@610 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/include/captcha/captchas/ttf_image.php
+++ wb/include/captcha/captchas/ttf_image.php
@@ -43,7 +43,7 @@ if(!function_exists('randomString')) {
 		list($usec, $sec) = explode(' ', microtime());
 		srand((float)$sec + ((float)$usec * 100000));
 		//$possible="ABCDEFGHJKLMNPRSTUVWXYZabcdefghkmnpqrstuvwxyz23456789";
-		$possible="abdfhkrsvwxyz23456789";
+		$possible="abdfhkrsvwxz23456789";
 		$str="";
 		while(strlen($str)<$len) {
 			$str.=substr($possible,(rand()%(strlen($possible))),1);
@@ -61,29 +61,65 @@ $bg = $bgs[array_rand($bgs)];
 list($width, $height, $type, $attr) = getimagesize($bg);
 
 // create image
-$image_failed = true;
-$i=0;
-do {
-	$image = ImageCreateFromPNG($bg); // backgroundimage
-	$grey = rand(0,50);
-	$color = ImageColorAllocate($image, $grey, $grey, $grey); // Farbe
-	$ttf = $font;
-	$ttfsize = 25; // fontsize
-	$angle = rand(0,5);
-	$t_x = rand(5,30);
-	$t_y = rand($height-10,$height-2);
-	$res = imagettftext($image, $ttfsize, $angle, $t_x, $t_y, $color, $ttf, $text);
-	// check if text fits into the image
-	if(($res[0]>0 && $res[0]<$width) && ($res[1]>0 && $res[1]<$height) && 
-	   ($res[2]>0 && $res[2]<$width) && ($res[3]>0 && $res[3]<$height) && 
-	   ($res[4]>0 && $res[4]<$width) && ($res[5]>0 && $res[5]<$height) && 
-	   ($res[6]>0 && $res[6]<$width) && ($res[7]>0 && $res[7]<$height)
-	) {
-		$image_failed = false;
-	}
-	if(++$i > 5) // too many tries! Use the image
-		break;
-} while($image_failed);
+
+$image = ImageCreateFromPNG($bg); // background image
+$grey = rand(0,50);
+$color = ImageColorAllocate($image, $grey, $grey, $grey); // font-color
+$ttf = $font;
+$ttfsize = 25; // fontsize
+$angle = rand(-15,15);
+$x = rand(10,25);
+$y = rand($height-10,$height-2);
+
+if(rand(0,2)==0) { // 1 out of 3
+
+	// draw each character individualy
+	$count = 0;
+	$image_failed = true;
+	do {
+		for($i=0;$i<5;$i++) {
+			$res = imagettftext($image, $ttfsize, $angle, $x, $y, $color, $ttf, $text{$i});
+			$angle = rand(-15,15);
+			$x = rand($res[4],$res[4]+10);
+			$y = rand($height-15,$height-5);
+		}
+		if($res[4] > $width) {
+			$image_failed = true;
+		} else {
+			$image_failed = false;
+		}
+		if(++$count > 4) // too many tries! Use the image
+			break;
+	} while($image_failed);
+	
+} else {
+	
+	// draw whole string at once
+	$image_failed = true;
+	$count=0;
+	do {
+		$image = ImageCreateFromPNG($bg); // background image
+		$grey = rand(0,50);
+		$color = ImageColorAllocate($image, $grey, $grey, $grey); // font-color
+		$ttf = $font;
+		$ttfsize = 25; // fontsize
+		$angle = rand(0,5);
+		$t_x = rand(5,30);
+		$t_y = rand($height-10,$height-2);
+		$res = imagettftext($image, $ttfsize, $angle, $t_x, $t_y, $color, $ttf, $text);
+		// check if text fits into the image
+		if(($res[0]>0 && $res[0]<$width) && ($res[1]>0 && $res[1]<$height) && 
+			 ($res[2]>0 && $res[2]<$width) && ($res[3]>0 && $res[3]<$height) && 
+			 ($res[4]>0 && $res[4]<$width) && ($res[5]>0 && $res[5]<$height) && 
+			 ($res[6]>0 && $res[6]<$width) && ($res[7]>0 && $res[7]<$height)
+		) {
+			$image_failed = false;
+		}
+		if(++$count > 5) // too many tries! Use the image
+			break;
+	} while($image_failed);
+	
+}
 
 captcha_header();
 ob_start();
