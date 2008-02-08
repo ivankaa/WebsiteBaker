cc9027ef8dbd157c2a0eed07508b0fc968cd9d78
thorn <thorn@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2008-02-08 17:00:17 +0000

    removed non-ascii character from source
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@671 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/framework/functions.php
+++ wb/framework/functions.php
@@ -830,7 +830,7 @@ if(!function_exists('get_variable_content')) {
 		// the variable name is returned in $match[1], the content in $match[3]
 		if (preg_match('/(\$' .$search .')\s*=\s*("|\')(.*)\2\s*;/', $data, $match)) {
 			if(strip_tags(trim($match[1])) == '$' .$search) {
-				// variable name matches, return it´s value
+				// variable name matches, return it's value
 				$match[3] = ($striptags == true) ? strip_tags($match[3]) : $match[3];
 				$match[3] = ($convert_to_entities == true) ? htmlentities($match[3]) : $match[3];
 				return $match[3];
