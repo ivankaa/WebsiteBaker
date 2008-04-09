2e6e38b3a202cd23b6a516c659ca46f364c428e6
doc <doc@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2008-04-09 18:49:59 +0000

    Modified FCKEditor WB-Link Plugin to work also with WB versions below 2.7
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@818 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/modules/fckeditor/fckeditor/editor/plugins/WBModules/fck_wbmodules.php
+++ wb/modules/fckeditor/fckeditor/editor/plugins/WBModules/fck_wbmodules.php
@@ -1,5 +1,28 @@
 <?php
 
+// $Id$
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2008, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
 // Include the config file
 require('../../../../../../config.php');
 
@@ -50,7 +73,8 @@ $get_pages = $database->query("SELECT * FROM ".TABLE_PREFIX."pages WHERE parent 
 if($get_pages->numRows() > 0) {
 	// Loop through pages
 	while($page = $get_pages->fetchRow()) {
-		if(!$admin->page_is_visible($page))
+		// method page_is_visible was introduced with WB 2.7
+		if(method_exists($admin, 'page_is_visible') && !$admin->page_is_visible($page))
 			continue;
 		$title = stripslashes($page['menu_title']);
 		$template->set_var('TITLE', $title);
