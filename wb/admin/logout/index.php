7b3d6474bf66977393be6d291975a329c68c2f65
doc <doc@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2009-02-16 18:36:44 +0000

    reworked logout to reset possible remember key in database when logging out
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@932 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/admin/logout/index.php
+++ wb/admin/logout/index.php
@@ -23,23 +23,40 @@
 
 */
 
-require("../../config.php");
+require('../../config.php');
 
-if(isset($_COOKIE['REMEMBER_KEY'])) {
-	setcookie('REMEMBER_KEY', '', time()-3600, '/');
+// delete remember key of current user from database
+if (isset($_SESSION['USER_ID']) && isset($database)) {
+	$table = TABLE_PREFIX . 'users';
+	$sql = "UPDATE `$table` SET `remember_key` = '' WHERE `user_id` = '" . (int) $_SESSION['USER_ID'] . "'";
+	$database->query($sql);
 }
 
+// delete remember key cookie if set
+if (isset($_COOKIE['REMEMBER_KEY'])) {
+	setcookie('REMEMBER_KEY', '', time() - 3600, '/');
+}
+
+// delete most critical session variables manually
 $_SESSION['USER_ID'] = null;
 $_SESSION['GROUP_ID'] = null;
 $_SESSION['GROUPS_ID'] = null;
 $_SESSION['USERNAME'] = null;
 $_SESSION['PAGE_PERMISSIONS'] = null;
 $_SESSION['SYSTEM_PERMISSIONS'] = null;
+
+// overwrite session array
 $_SESSION = array();
-session_unset();
-unset($_COOKIE[session_name()]);
+
+// delete session cookie if set
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+
+// delete the session itself
 session_destroy();
 
-header("Location: ".ADMIN_URL."/login/index.php");
+// redirect to admin login
+die(header('Location: ' . ADMIN_URL . '/login/index.php'));
 
 ?>
