ef013be352d6973690fc9e466b7264bb89353edb
Ruebenwurzel <Ruebenwurzel@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2009-06-29 19:50:56 +0000

    Update PHPMailer to version 2.0.4 (ticket #733)
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@1020 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/include/phpmailer/language/phpmailer.lang-no.php
+++ wb/include/phpmailer/language/phpmailer.lang-no.php
@@ -6,20 +6,18 @@
 
 $PHPMAILER_LANG = array();
 
-$PHPMAILER_LANG["provide_address"]      = 'Du m&aring; angi e-post adresse ' .
-                                          'til minst en mottaker .';
-$PHPMAILER_LANG["mailer_not_supported"] = ' mailer st&oslash;ttes ikke.';
-$PHPMAILER_LANG["execute"]              = 'Opperasjonen kunne ikke utf&oslash;res: ';
-$PHPMAILER_LANG["instantiate"]          = 'E-post funksjonen kunne ikke startes.';
-$PHPMAILER_LANG["authenticate"]         = 'SMTP Feil: Autentifikasjon kunne oppn&aring;es.';
-$PHPMAILER_LANG["from_failed"]          = 'Det oppstod en feil med denne Fra adressen: ';
-$PHPMAILER_LANG["recipients_failed"]    = 'SMTP Feil: Feil med følgende ' .
-                                          'e-post mottaker: ';
-$PHPMAILER_LANG["data_not_accepted"]    = 'SMTP Feil: Dataene ble ikke akseptert.';
-$PHPMAILER_LANG["connect_host"]         = 'SMTP Feil: Kunne ikke koble til SMTP verten.';
-$PHPMAILER_LANG["file_access"]          = 'Fikk ikke tillgang til filen: ';
-$PHPMAILER_LANG["file_open"]            = 'Fil Feil: Filen kunne ikke &aring;pnes: ';
-$PHPMAILER_LANG["encoding"]             = 'Ukjent kodingstype: ';
-$PHPMAILER_LANG["signing"]              = 'Signeringsfeil: ';
-
+$PHPMAILER_LANG['provide_address']      = 'Du må ha med minst en mottager adresse.';
+$PHPMAILER_LANG['mailer_not_supported'] = ' mailer er ikke supportert.';
+$PHPMAILER_LANG['execute']              = 'Kunne ikke utføre: ';
+$PHPMAILER_LANG['instantiate']          = 'Kunne ikke instantiate mail funksjonen.';
+$PHPMAILER_LANG['authenticate']         = 'SMTP Feil: Kunne ikke authentisere.';
+$PHPMAILER_LANG['from_failed']          = 'Følgende Fra feilet: ';
+$PHPMAILER_LANG['recipients_failed']    = 'SMTP Feil: Følgende mottagere feilet: ';
+$PHPMAILER_LANG['data_not_accepted']    = 'SMTP Feil: Data ble ikke akseptert.';
+$PHPMAILER_LANG['connect_host']         = 'SMTP Feil: Kunne ikke koble til SMTP host.';
+$PHPMAILER_LANG['file_access']          = 'Kunne ikke få tilgang til filen: ';
+$PHPMAILER_LANG['file_open']            = 'Fil feil: Kunne ikke åpne filen: ';
+$PHPMAILER_LANG['encoding']             = 'Ukjent encoding: ';
+$PHPMAILER_LANG['signing']              = 'Signing Error: ';
+$PHPMAILER_LANG['smtp_error']           = 'SMTP server error: ';
 ?>
