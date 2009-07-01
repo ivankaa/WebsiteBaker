13301899fc1d9c61f9f70823065cf8a5b23cc489
Ruebenwurzel <Ruebenwurzel@98bf1eab-f5fc-0310-a3ed-aac8851f7404>
2009-07-01 07:01:04 +0000

    Set Version to WB2.8RC1
    git-svn-id: svn://isteam.dynxs.de/wb_svn/wb280/trunk@1025 98bf1eab-f5fc-0310-a3ed-aac8851f7404

--- wb/admin/interface/version.php
+++ wb/admin/interface/version.php
@@ -37,6 +37,6 @@ if(!defined('WB_URL')) {
 }
 
 // check if defined to avoid errors during installation (redirect to admin panel fails if PHP error/warnings are enabled)
-if(!defined('VERSION')) define('VERSION', '2.8 BETA');
+if(!defined('VERSION')) define('VERSION', '2.8RC1');
 
 ?>
